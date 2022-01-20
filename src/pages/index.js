605eb1abe1a500b97bc0aed9a20f4d70291427df
Felipe Moreira <ti.felipemoreira@gmail.com>
2022-01-20 19:28:49 -0300

    navbar

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+
+export default function Home() {
+  return (
+    <div >
+      <Navbar></Navbar>
+    </div>
+  )
+}
