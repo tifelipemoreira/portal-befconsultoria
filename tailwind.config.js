ee186addac2d253664f6662277dd0cbd27f07b72
Felipe Moreira <ti.felipemoreira@gmail.com>
2022-01-22 20:03:06 -0300

    ajuste tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  purge: [
+  content: [
     ".src//pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: false,
+  darkMode: `media`,
   theme: {
     extend: {},
   },
